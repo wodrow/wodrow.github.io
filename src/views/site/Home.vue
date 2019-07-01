bb2315c0412f1ad43cf890babaccea73550ba738
wodrow-ys-vm-2008 <wodrow451611cv@gmail.com>
2019-07-01 17:08:06 +0800

    add home

--- src/views/site/Home.vue
+++ src/views/site/Home.vue
@@ -13,36 +13,34 @@
             <b-col md="6">
                 <div class="card flex-md-row mb-4 box-shadow h-md-250">
                     <div class="card-body d-flex flex-column align-items-start">
-                        <strong class="d-inline-block mb-2 text-primary">World</strong>
+                        <strong class="d-inline-block mb-2 text-primary">电影推荐</strong>
                         <h3 class="mb-0">
-                            <a class="text-dark" href="#">Featured post</a>
+                            <div class="text-dark">阿甘正传</div>
                         </h3>
-                        <div class="mb-1 text-muted">Nov 12</div>
-                        <p class="card-text mb-auto">This is a wider card with supporting text below as a natural
-                            lead-in to additional content.</p>
-                        <a href="#">Continue reading</a>
+                        <div class="mb-1 text-muted">1994</div>
+                        <p class="card-text mb-auto">人生就像一盒巧克力,你永远也不知道下一个吃到的是什么味道。</p>
+                        <a href="https://www.iqiyi.com/v_19rrk3u264.html?vfm=2008_aldbd&fc=828fb30b722f3164&fv=p_02_01" target="_blank">去观看</a>
                     </div>
                     <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb"
                          alt="Thumbnail [200x250]" style="width: 200px; height: 250px;"
-                         src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16b9d730b43%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16b9d730b43%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2255.609375%22%20y%3D%22131%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+                         src="~@/assets/agzz.jpg"
                          data-holder-rendered="true">
                 </div>
             </b-col>
             <b-col md="6">
                 <div class="card flex-md-row mb-4 box-shadow h-md-250">
                     <div class="card-body d-flex flex-column align-items-start">
-                        <strong class="d-inline-block mb-2 text-success">Design</strong>
+                        <strong class="d-inline-block mb-2 text-success">书籍推荐</strong>
                         <h3 class="mb-0">
-                            <a class="text-dark" href="#">Post title</a>
+                            <div class="text-dark">人性的弱点</div>
                         </h3>
-                        <div class="mb-1 text-muted">Nov 11</div>
-                        <p class="card-text mb-auto">This is a wider card with supporting text below as a natural
-                            lead-in to additional content.</p>
-                        <a href="#">Continue reading</a>
+                        <div class="mb-1 text-muted">2006</div>
+                        <p class="card-text mb-auto">时常保持微笑”“牢记每个人的名字”“做一个善于聆听的人”“永远让别人感到重要”</p>
+                        <a href="http://www.renxingruodian.com/" target="_blank">去阅读</a>
                     </div>
                     <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb"
                          alt="Thumbnail [200x250]"
-                         src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16b9d730b45%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16b9d730b45%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2255.609375%22%20y%3D%22131%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+                         src="~@/assets/rxdrd.jpg"
                          data-holder-rendered="true" style="width: 200px; height: 250px;">
                 </div>
             </b-col>
